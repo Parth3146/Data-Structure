a561a4d2ae6f1fdfeaf232d6faa6b500fc4d1ef4
Om pandey <54907469+pandeyom331@users.noreply.github.com>
2020-10-01 11:29:29 +0530

    Update structure.c

--- structure.c
+++ structure.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include<stdio.h> //Header file
 
-struct employee
+struct employee   // Initializing structure
 {
 	char name[20];
 	int id,pno,salary;
-}e[5];
+}e[5];          // we can call structure employee by e[i]
 
-void main()
+void main()  // Main fuction of void return type
 {
 	int i;
-	for(i=0;i<=4;i++)
+	for(i=0;i<=4;i++)     // for-loop for taking input from user 
 	{
 		printf("Enter %d employee's name: ",(i+1));
 		scanf("%s",e[i].name);
@@ -21,7 +21,7 @@ void main()
 		scanf("%d",&e[i].salary);
 	}
 	printf("Employee's Details are:\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=4;i++)   // for-loop for printing details of employee
 	{
 		printf("%d.  Name:         %s\n",(i+1),e[i].name);
 		printf("    ID No.:       %d\n",e[i].id);
